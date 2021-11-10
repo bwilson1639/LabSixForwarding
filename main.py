ac53ac1c7533b10b25e96a7b707aa7f5a4980739
Benjamin <bwilson1@ualberta.ca>
2021-11-10 16:28:04 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+
+class KB:
+
+    def __init__(self):
+        self.premise = [""]
+        self.conclusion = ""
+        self.count = 0
+inferred = {}
+agenda = [""]
+knowledgeBase = []
+
+def userInput():
+    'takes in the user input and saves it to the knowledge base and agenda'
+
+    while True:
+
+        print("Please input knowledge base:(type 'exit' if you want to stop)")
+        userInputtedData = input()
+
+        if userInputtedData == 'exit':
+            return 0
+
+        elif len(userInputtedData) == 1:
+            agenda.append(userInputtedData)
+
+        elif userInputtedData.find('^') == -1: #not found
+
+            newKB = KB()
+            newData = userInputtedData.split('=>')
+            newKB.count = 1
+            newKB.premise.append(newData[0])
+            newKB.conclusion = newData[1]
+
+            knowledgeBase.append(newKB)
+
+        else:
+
+            newKB = KB()
+            newData = userInputtedData.split('=>')
+
+            newKB.conclusion = newData[1]
+            premise = newData[0].split('^')
+
+            newKB.count = len(premise)
+
+            for i in len(premise):
+                newKB.premise.append(premise[i])
+
+
+userInput()
+print(knowledgeBase)
